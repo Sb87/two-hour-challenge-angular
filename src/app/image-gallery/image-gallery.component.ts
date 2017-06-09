b0b633a8afd2175bca0d10be37811b7202bd8fce
Steven Barnett <Sb87@users.noreply.github.com>
2017-06-09 12:39:08 -0700

    Added date picker to gallery. Added response caching to image service. Fixed JS error in console on IOTD.

--- src/app/image-gallery/image-gallery.component.ts
+++ src/app/image-gallery/image-gallery.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges } from '@angular/core';
+import {IMyDpOptions, IMyDateModel} from 'mydatepicker';
 
 @Component({
   selector: 'app-image-gallery',
   templateUrl: './image-gallery.component.html',
   styleUrls: ['./image-gallery.component.css']
 })
-export class ImageGalleryComponent implements OnInit {
+export class ImageGalleryComponent {
+  date?: IMyDateModel;
+  datePickerOptions: IMyDpOptions;
 
-  constructor() { }
+  constructor() {
+    const now = new Date();
+    now.setDate(now.getDate() + 1);
 
-  ngOnInit() {
+    this.datePickerOptions = {
+      width: '200px',
+      dateFormat: 'mm/dd/yyyy',
+      disableSince: { year: now.getFullYear(), month: now.getMonth() + 1, day: now.getDate() }
+    };
   }
-
 }
--- src/app/image.service.ts
+++ src/app/image.service.ts
@@ -12,15 +12,23 @@ const opportunityMaxDate = new Date(2017, 1, 22);
 
 @Injectable()
 export class ImageService {
+  private cache: { [roverDate: string]: Photo[] } = {};
+
   constructor(private http: Http) { }
 
-  // TODO: add caching
   getDailyImages(rover: RoverName, date: Date): Observable<Photo[]> {
-    const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?earth_date=${this.getDateString(date)}&api_key=${apiKey}`;
+    const dateString = this.getDateString(date);
+    const cachedResponse = this.cache[rover + dateString];
+    if (cachedResponse) {
+      return Observable.from([cachedResponse]);
+    }
+
+    const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?earth_date=${dateString}&api_key=${apiKey}`;
 
     return this.http.get(url).map(response => {
       if (response.status === 200) {
         const photosResponse = response.json() as MarsPhotosResponse;
+        this.cache[rover + dateString] = photosResponse.photos;
         return photosResponse.photos;
       }
       else {
@@ -52,7 +60,7 @@ export class ImageService {
   }
 
   private getPreviousDate(input?: Date): Date {
-    const output = input ? new Date(input) : new Date();
+    const output = input ? new Date(input.getTime()) : new Date();
     output.setDate(output.getDate() - 1);
     return output;
   }
